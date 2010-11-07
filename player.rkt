109d6fb96fefd168efc62ded4ad60fec061f3876
Asumu Takikawa <asumu@interchange.ubc.ca>
2010-11-07 16:00:47 -0500

    Add seeking, progress bars, and better playlist support.

--- player.rkt
+++ player.rkt
@@ -6,12 +6,15 @@
 	 (prefix-in taglib- "taglib/taglib.rkt")
 	 "gst/gst.rkt")
 
-(when (gstreamer-initialize)
-  (displayln "Initialization success"))
+(unless (gstreamer-initialize)
+  (error "Could not initialize GStreamer"))
 
-(define play-icon (make-object bitmap% "icons/media-playback-start.png" 'png))
-(define pause-icon (make-object bitmap% "icons/media-playback-pause.png" 'png))
-(define stop-icon (make-object bitmap% "icons/media-playback-stop.png" 'png))
+(define icon-color (make-object color% "white"))
+(define play-icon  (make-object bitmap% "icons/media-playback-start.png" 'png icon-color))
+(define pause-icon (make-object bitmap% "icons/media-playback-pause.png" 'png icon-color))
+(define stop-icon  (make-object bitmap% "icons/media-playback-stop.png" 'png icon-color))
+(define fwd-icon   (make-object bitmap% "icons/media-skip-forward.png" 'png icon-color))
+(define bwd-icon   (make-object bitmap% "icons/media-skip-backward.png" 'png icon-color))
 
 (define player-frame%
   (class frame%
@@ -26,8 +29,13 @@
          
          (define control-panel (new horizontal-panel%
                                     [parent this]
-                                    [stretchable-height #f]
-                                    [spacing 2]))
+                                    [stretchable-height #f]))
+
+         (define backward-button
+           (make-object button% bwd-icon control-panel
+                        (lambda (b e)
+                          (send playlist play-last))))
+
          (define play-button
            (make-object button% play-icon control-panel
                         (lambda (b e)
@@ -38,10 +46,33 @@
                         (lambda (b e)
                           (send player stop))))
          
+	 (define forward-button
+           (make-object button% fwd-icon control-panel
+                        (lambda (b e)
+                          (send playlist play-next))))
+         
          (define now-playing (new message%
                                   [label "No file"]
                                   [parent control-panel]
                                   [stretchable-width #t]))
+	
+	 (define progress (new slider%
+	                       [label #f]
+			       [min-value 0]
+			       [max-value 1000]
+			       [init-value 0]
+			       [style '(plain horizontal)]
+			       [parent control-panel]
+			       [callback
+			         (lambda (sl e)
+				   (send player set-progress
+				     (/ (send sl get-value) 1000)))]))
+
+         (define play-timer
+           (new timer% [notify-callback
+                        (lambda ()
+                          (send progress set-value 
+			    (floor (* 1000 (send player get-progress)))))]))
          
          (send now-playing auto-resize #t)
          
@@ -57,7 +88,7 @@
                         (lambda (mi evt)
                           (define path (finder:get-file))
                           (when path
-			    (send playlist push-and-play (new song% [path path]))))))
+			    (send playlist open-and-play (new song% [path path]))))))
          
          (define enqueue-item
            (make-object menu-item%
@@ -69,23 +100,30 @@
 			    (send playlist enqueue (new song% [path path]))))))
 
          (define/public (on-paused)
-           (send play-button set-label play-icon))
+           (send play-button set-label play-icon)
+	   (send play-timer stop))
          
          (define/public (on-playing)
-           (send play-button set-label pause-icon))
+           (send play-button set-label pause-icon)
+	   (send play-timer start 500))
+
+         (define/public (on-stopped)
+           (send play-button set-label play-icon)
+	   (send play-timer stop))
          
          (define/public (on-song-ended)
-	   (send now-playing set-label "")
-	   (set-status-text "")
-	   (send playlist remove-song))
+	   (send play-button set-label play-icon)
+	   (send play-timer stop)
+	   (send playlist play-next))
 
          (define/public (set-metadata metadata)
            (send now-playing set-label (taglib-tag-title metadata))
            (set-status-text
-            (string-append "Now playing: "
-                           (number->string (taglib-tag-track metadata))
-                           " - " (taglib-tag-title metadata)
-                           " (" (taglib-tag-artist metadata) ")")))))
+            (format "Length: ~a Bitrate: ~a Samplerate: ~a Channels: ~a"
+	      (taglib-tag-length metadata)
+	      (taglib-tag-bitrate metadata)
+	      (taglib-tag-samplerate metadata)
+	      (taglib-tag-channels metadata))))))
 
 (define player%
   (class object%
@@ -105,7 +143,8 @@
                     (let ([st (current-state)])
                       (case st
                         ['paused (send ui on-paused)]
-                        ['playing (send ui on-playing)]))]
+                        ['playing (send ui on-playing)]
+			['null (send ui on-stopped)]))]
                    ['eos (begin
 		          (stop)
                           (send ui on-song-ended))])
@@ -122,6 +161,13 @@
          
          (define (paused? st)
            (eq? st 'paused))
+
+	 (define/public (get-progress)
+	   (/ (send pb get-position)
+	      (send pb get-duration)))
+
+	 (define/public (set-progress pct)
+	   (send pb seek-simple 'flush (* pct (send pb get-duration))))
          
          (define/public (toggle-pause-play)
            (let ([st (current-state)])
@@ -139,9 +185,9 @@
            (send pb set-state 'paused))
          
          (define/public (set-next-song song)
+           (stop)
 	   (send pb set-uri (path->uri (get-field path song)))
            (send ui set-metadata (get-field metadata song))
-           (stop)
            (play))))
 
 (define song%
@@ -156,52 +202,58 @@
   (class list-box%
     (init-field player parent)
     (inherit append delete get-data set-string
-             get-number set-data get-string)
+             get-number set-data get-string clear
+	     set-selection get-selection)
+
+    (define (click-callback pl evt)
+      ;; this event type never seems to trigger
+      (when (eq? (send evt get-event-type) 'list-box-dclick)
+	(set! index (get-selection))
+	(play-current)))
 
     (super-new [label #f]
                [choices '()]
-	       [parent parent])
+	       [parent parent]
+	       [callback click-callback])
+
+    (define current-song #f)
+    ;; the current position in the playlist
+    (define index 0)
 
     ;; -> song% or #f
-    ;; gets the head of the queue
     (define/public (get-current-song)
-      (if (zero? (get-number))
-          #f
-	  (get-data 0)))
+      current-song)
 
     ;; song% -> void?
-    ;; called to play a new song as the head of the queue
-    (define/public (push-and-play song)
-      (let ([tag (get-field metadata song)])
-        (push-on-front (taglib-tag-title tag) song)
-	(play-next)))
-
-    ;; push to front of list-box choices
-    (define (push-on-front str data)
-      (if (zero? (get-number))
-	  (append str data)
-	  (let* ([temp-str (get-string 0)]
-	         [temp-dat (get-data 0)])
-	    (begin
-	      (set-string 0 str)
-	      (set-data 0 data)
-	      (set-string 1 temp-str)
-	      (set-data 1 temp-dat)))))
+    ;; called to play a new song, ignore the playlist
+    (define/public (open-and-play song)
+      (let* ([tag (get-field metadata song)]
+             [title (taglib-tag-title tag)])
+        (clear)
+        (set! index 0)
+	(append title song)
+	(play-current)))
 
     ;; song% -> void?
     ;; enqueue the next song (at the end)
     (define/public (enqueue song)
       (let ([tag (get-field metadata song)])
 	(append (taglib-tag-title tag) song)))
-
-    (define/public (remove-song)
-      (when (not (zero? (get-number)))
-	(delete 0)
-	(play-next)))
+    
+    (define/public (play-last)
+      (when (> index 0)
+	(set! index (sub1 index))
+	(play-current)))
 
     (define/public (play-next)
+      (when (< (+ index 1) (get-number))
+	(set! index (add1 index))
+	(play-current)))
+
+    (define/public (play-current)
       (when (not (zero? (get-number)))
-	(let* ([next-song (get-data 0)])
+	(let ([next-song (get-data index)])
+	  (set-selection index)
 	  (send player set-next-song next-song))))))
 
 (define ui (new player-frame%))
